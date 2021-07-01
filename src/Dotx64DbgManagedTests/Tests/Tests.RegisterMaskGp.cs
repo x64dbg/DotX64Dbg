1b98c93409513c82780d859556aeec3db2f2d490
ZehMatt <m.moninger.h@gmail.com>
2021-07-02 01:02:13 +0300

    Add more tests for register masks

--- src/Dotx64DbgManagedTests/Tests/Tests.RegisterMaskGp.cs
+++ src/Dotx64DbgManagedTests/Tests/Tests.RegisterMaskGp.cs
@@ -81,5 +81,119 @@ namespace Dotx64Dbg.Managed.Tests
                 AssertEq(regs2[i].Value, regs[i]);
             }
         }
+
+        [Test]
+        public void TestMaskCountForward()
+        {
+            Register[] regs = new[] {
+                Register.Eax,
+                Register.Ebx,
+                Register.Ecx,
+                Register.Edx,
+                Register.Ebp,
+                Register.Esp,
+                Register.Edi,
+                Register.R8d,
+                Register.R9d,
+                Register.R10d,
+                Register.R11d,
+                Register.R12d,
+                Register.R13d,
+                Register.R14d,
+                Register.R15d
+            };
+
+            var mask = Analysis.RegisterMaskGp.None;
+            var i = 0;
+            foreach (var reg in regs)
+            {
+                mask.Add(reg);
+
+                i++;
+                AssertEq(mask.Count, i);
+            }
+
+        }
+
+        [Test]
+        public void TestMaskCountReverse()
+        {
+            List<Register> regs = new()
+            {
+                Register.Eax,
+                Register.Ebx,
+                Register.Ecx,
+                Register.Edx,
+                Register.Ebp,
+                Register.Esp,
+                Register.Edi,
+                Register.R8d,
+                Register.R9d,
+                Register.R10d,
+                Register.R11d,
+                Register.R12d,
+                Register.R13d,
+                Register.R14d,
+                Register.R15d
+            };
+
+            var mask = Analysis.RegisterMaskGp.None;
+            var i = 0;
+
+            regs.Reverse();
+            foreach (var reg in regs)
+            {
+                mask.Add(reg);
+
+                i++;
+                AssertEq(mask.Count, i);
+            }
+
+        }
+
+        [Test]
+        public void TestMaskCountRandom()
+        {
+            List<Register> regs = new()
+            {
+                Register.Eax,
+                Register.Ebx,
+                Register.Ecx,
+                Register.Edx,
+                Register.Ebp,
+                Register.Esp,
+                Register.Edi,
+                Register.R8d,
+                Register.R9d,
+                Register.R10d,
+                Register.R11d,
+                Register.R12d,
+                Register.R13d,
+                Register.R14d,
+                Register.R15d
+            };
+
+            for (int seed = 0; seed < 100; seed++)
+            {
+                var mask = Analysis.RegisterMaskGp.None;
+                var i = 0;
+                var prng = new System.Random(seed);
+
+                while (regs.Count > 0)
+                {
+                    var idx = prng.Next(0, regs.Count - 1);
+                    var reg = regs[idx];
+                    regs.RemoveAt(idx);
+
+                    mask.Add(reg);
+
+                    i++;
+                    AssertEq(mask.Count, i);
+                }
+            }
+
+        }
+
+
     }
 }
