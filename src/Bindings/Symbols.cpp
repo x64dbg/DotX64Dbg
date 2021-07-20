bc16d1d8db2557fba1fd2f70e4255fc60c921f3f
ZehMatt <m.moninger.h@gmail.com>
2021-07-20 17:10:50 +0300

    Add bindings for some symbols

---
+++ src/Bindings/Symbols.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstdint>
+#include <utility>
+#include <vector>
+
+#include "pluginsdk/bridgemain.h"
+#include "pluginsdk/_plugins.h"
+#include "pluginsdk/_scriptapi_label.h"
+#include "Symbols.hpp"
+
+namespace Dotx64Dbg {
+
+
+}
